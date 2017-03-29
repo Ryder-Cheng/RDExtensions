38b699774b8b11349692881de25fd8c08f4a4c17
ChengYaojun <chengyaojun@gmail.com>
2017-03-29 21:49:59 +0800

    fix bug

--- RDExtensions/RDExtensions.h
+++ RDExtensions/RDExtensions.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char RDExtensionsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RDExtensions/PublicHeader.h>
 
-
+#import "UIColor+Extension.h"
+#import "UIView+Extension.h"
